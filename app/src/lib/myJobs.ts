77e77443538d87562e7e95e249813893a52db9a1
lily page <lily@nyan.ca>
2023-04-19 01:37:52 +0100

    preloading

--- app/src/lib/myJobs.ts
+++ app/src/lib/myJobs.ts
@@ -1,5 +1,7 @@
-import { writable } from 'svelte/store';
+import { writable, get as getStore } from 'svelte/store';
 import { crossfade as svelteCrossfade } from 'svelte/transition';
+import { get } from './requestUtils';
+import type { Job } from '$interfaces/job';
 
 const [send, receive] = svelteCrossfade({duration: 400});
 
@@ -14,6 +16,23 @@ interface ApplicationStore {
     send: any,
     receive: any
 }
+
+export const jobQueue = writable<string[]>([])
+
+export async function popNextJobID() {
+    if (getStore(jobQueue).length > 0) {
+        const id = getStore(jobQueue)[0]
+        jobQueue.update(x => x.splice(1))
+        return id
+    } else {
+        const newJobs: Job[] = await get("/jobs")
+        jobQueue.set(newJobs.map(j => j.id))
+        const id = getStore(jobQueue)[0]
+        jobQueue.update(x => x.splice(1))
+        return id
+    }
+}
+
 
 export const applications = writable<ApplicationStore>({
     applications: [],
