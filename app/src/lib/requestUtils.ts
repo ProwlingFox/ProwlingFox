bd2b34630092e6d58ffb0237fc30822e151d9449
Lia Neville <lianeville5@gmail.com>
2023-05-02 04:41:39 -0500

    fixes ipv6 issues

--- app/src/lib/requestUtils.ts
+++ app/src/lib/requestUtils.ts
@@ -2,10 +2,10 @@ import cookie from 'cookie'
 import { browser } from '$app/environment'
 import { readable, get as getStore } from 'svelte/store'
 import type { Readable } from 'svelte/store'
-import jwt_decode from "jwt-decode";
+import jwt_decode from 'jwt-decode'
 
-const apiUrl = "http://localhost:8000"
-let JWTStore: Readable<string>;
+const apiUrl = 'http://127.0.0.1:8000'
+let JWTStore: Readable<string>
 
 function get(path: string) {
 	return makeRequest('GET', path)
@@ -33,10 +33,8 @@ async function makeRequest(method: string, path: string, body?: object) {
 			body: body ? JSON.stringify(body) : undefined,
 			headers: headers,
 		})
-		if (response.ok)
-			return response.json()
-		else 
-			throw response
+		if (response.ok) return response.json()
+		else throw response
 	} catch (error) {
 		console.error('Fetch Request Failed', error)
 		return {}
@@ -45,17 +43,17 @@ async function makeRequest(method: string, path: string, body?: object) {
 
 function setJWT(JWT: string) {
 	// For Setting The JWT
-	if (browser) {	//Don't Set On Browser
-		return null;
+	if (browser) {
+		//Don't Set On Browser
+		return null
 	}
 
 	JWTStore = readable(JWT)
-
 }
 
 function getJWT() {
 	// Code is different on server vs client
-	if (browser){
+	if (browser) {
 		let cookies = cookie.parse(document.cookie)
 		return cookies.token
 	} else {
@@ -64,16 +62,18 @@ function getJWT() {
 }
 
 interface DecodedJWT {
-	expiry: number,
-	permission: string,
-	user_id: string,
-	name: string,
+	expiry: number
+	permission: string
+	user_id: string
+	name: string
 	email: string
 }
 
 function parseJWT(): DecodedJWT | null {
 	let JWT = getJWT()
-	if (!JWT) { return null }
+	if (!JWT) {
+		return null
+	}
 	var decoded = jwt_decode<DecodedJWT>(JWT)
 	return decoded
 }
