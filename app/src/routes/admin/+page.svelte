524a690388a8756a4b952a13d6a53839956d0587
lily page <lily@nyan.ca>
2023-05-24 19:00:06 +0100

    Charts!

--- app/src/routes/admin/+page.svelte
+++ app/src/routes/admin/+page.svelte
@@ -29,6 +29,7 @@
     parseJWT()
     let jobInjestCost: HTMLCanvasElement | undefined
     let jobInjestCount: HTMLCanvasElement | undefined
+    let applicationCost: HTMLCanvasElement | undefined
 
     let metrics: Metrics = data.metrics
     let users: User[] = data.users
@@ -55,10 +56,27 @@
 
     console.log(dateGroupedOpenAI)
 
-    const options: ChartOptions<'line'> = {
+    const options: ChartOptions<'line'|'bar'> = {
         scales: {
             x: {
                 type: 'linear',
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, ticks) {
+                        const date =  new Date(value);
+                        const day = date.getDate();
+                        return `${date.getDate()}/${date.getMonth()}`;
+
+                    }
+                }
+            },
+            y: {
+                ticks: {
+                    // Include a dollar sign in the ticks
+                    callback: function(value, index, ticks) {
+                        return '$' + value;
+                    }
+                }
             }
         },
         elements: {
@@ -71,8 +89,11 @@
     $: if(jobInjestCount) {
         new Chart(jobInjestCount,
             {
-                type: 'line',
-                data: getDataFromOpenAIUsage('Count Of Jobs Injested', x => x.MatchRole?.count ?? 0),
+                type: 'bar',
+                data: getDataFromOpenAIUsage([{
+                    label: 'Count Of Jobs Injested', 
+                    fn: x => x.MatchRole?.count ?? 0
+                }]),
                 options: options
             }
         );
@@ -83,30 +104,44 @@
             {
                 type: 'line',
                 // Currency Calc In $
-                data: getDataFromOpenAIUsage('$ Per 1000 Jobs Injested', x => {
-                    return (
-                    (x.MatchRole?.total_tokens ?? 0)                * 0.0004 +
-                    (x.multiPreprocess?.total_tokens ?? 0)          * 0.002 +
-                    (x.summarizeJobDescription?.total_tokens ?? 0)  * 0.002 + 
-                    (x.roleRequirements?.total_tokens ?? 0)         * 0.002 + 
-                    (x.roleKeyPoints?.total_tokens ?? 0)            * 0.002 + 
-                    (x.questionPreprocessor?.total_tokens ?? 0)     * 0.002
-                    ) / x.multiPreprocess?.count
-                }),
+                data: getDataFromOpenAIUsage([
+                    {
+                        label: '$ Per 1000 Jobs Injested', 
+                        fn: x => {
+                        return (
+                            (x.MatchRole?.total_tokens ?? 0)                * 0.0004 +
+                            (x.multiPreprocess?.total_tokens ?? 0)          * 0.002 +
+                            (x.summarizeJobDescription?.total_tokens ?? 0)  * 0.002 + 
+                            (x.roleRequirements?.total_tokens ?? 0)         * 0.002 + 
+                            (x.roleKeyPoints?.total_tokens ?? 0)            * 0.002 + 
+                            (x.questionPreprocessor?.total_tokens ?? 0)     * 0.002
+                            ) / x.multiPreprocess?.count
+                        }
+                    },
+                    {
+                        label: '$ Per 1000 Applications', 
+                        fn: x => {
+                            return (metrics.averageAIQuestionsPerJob * (x.answerQuestion?.total_tokens ?? 0) * 0.002) / x.answerQuestion?.count
+                        },
+                    }
+                ]),
                 options: options
             }
         );
     }
+
+
     
-    function getDataFromOpenAIUsage(label: string, fn: (x: {[key:string]: OpenAIUsage}) => any) {
+    function getDataFromOpenAIUsage(data: {label: string, fn: (x: {[key:string]: OpenAIUsage}) => any}[]) {
         return {
             labels: Object.keys(dateGroupedOpenAI).sort((a, b) => parseInt(b) - parseInt(a)),
-            datasets: [
-            {
-                label: label,
-                data: Object.values(dateGroupedOpenAI).sort((a, b) => b.date.getTime() - a.date.getTime()).map(x => x.openAiUsage).map(fn)
-            }
-            ]
+            datasets: data.map(y => {
+                return {
+                    label: y.label,
+                    data: Object.values(dateGroupedOpenAI).sort((a, b) => b.date.getTime() - a.date.getTime()).map(x => x.openAiUsage).map(y.fn),
+                    spanGaps: true
+                }
+            })
         }
     }
 
@@ -114,7 +149,7 @@
 
 </script>
 
-<div>
+<div class="overflow-scroll w-full">
     <div class="bg-white p-6 m-4 flex items-center gap-4 shadow-md rounded-xl">
         <h2>Active Jobs Processed/Scraped</h2>
         <p class="metric">{metrics.processedJobsCount}/{metrics.activeJobsCount}</p>
@@ -143,11 +178,14 @@
         </div>
     </div>
 
-    <div>
-        <canvas bind:this={jobInjestCount}></canvas>
-    </div>
-    <div>
-        <canvas bind:this={jobInjestCost}></canvas>
+    <!-- Charts have a 2:1 Aspect ratio -->
+    <div class="flex flex-wrap h-fit aspect-[1] md:aspect-[4/1]">
+        <span class="w-full h-0 md:w-1/2">
+            <canvas bind:this={jobInjestCount}></canvas>
+        </span>
+        <span class="w-full h-0 md:w-1/2">
+            <canvas bind:this={jobInjestCost}></canvas>
+        </span>
     </div>
 </div>
 
